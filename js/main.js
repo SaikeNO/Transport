d39108cfabe5c9a5ca8f08f6c7ca0803ddc31b8a
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2021-05-19 17:02:40 +0200

    feat: Implement about and numbers section

--- js/main.js
+++ js/main.js
@@ -44,4 +44,42 @@ const slider = () => {
   });
 };
 
+const counter = () => {
+  const clientNumberElem = document.getElementById("client-number");
+  const ratePercentElem = document.getElementById("rate-percent");
+  const ratingNumberElem = document.getElementById("rating-number");
+
+  const distanceFromTop = clientNumberElem.offsetTop;
+  const windowHeight = window.innerHeight;
+
+  let clientNumber = 0;
+  let ratePercent = 0;
+  let ratingNumber = 0;
+  let flaga = true;
+
+  window.addEventListener("scroll", () => {
+    let scrollValue = window.scrollY;
+    if (distanceFromTop < scrollValue + windowHeight && flaga) {
+      flaga = false;
+      const indexInterval = setInterval(() => {
+        if (clientNumber < 42) {
+          clientNumber++;
+        }
+        if (ratePercent < 73) {
+          ratePercent++;
+        }
+        if (ratingNumber < 4.9) {
+          ratingNumber += 0.1;
+        }
+        clientNumberElem.textContent = `${clientNumber}+`;
+        ratePercentElem.textContent = `${ratePercent}%`;
+        ratingNumberElem.textContent = `${ratingNumber.toFixed(2)}`;
+        if (clientNumber === 42 && ratePercent === 73 && ratingNumber === 5.0)
+          clearInterval(indexInterval);
+      }, 40);
+    }
+  });
+};
+
 slider();
+counter();
